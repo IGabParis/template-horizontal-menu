385eda5a13d8fa524d6cfd9e5e5fb361784e5d85
Iliana Paris <creativegroupagp@gmail.com>
2019-02-03 00:50:42 -0300

    Add files via upload

---
+++ app/view/includes/footer.php
@@ -0,0 +1,16 @@
+        <footer class="footer-black">
+            <span class="">Usuario: <?php echo $_SESSION['usuario'].' / '.$_SESSION['nombre'].' '.$_SESSION['apellido']; ?> <small><a href="../perfil/ver.perfil.php">Ver Perfil</a></small> - <small><a href="../../controller/index.controller/cerrar.sesion.php">Cerrar Sesion</a></small> </span>
+             <br>
+            <small>Temp Admin - IGabParis 2019</small>
+        </footer>
+        <script src="../../../assets/js/bootstrap.min.js"></script>
+        <script src="../../../assets/js/jquery-ui-1.10.4.min.js"></script>
+        <script src="../../../assets/lib/datepicker/js/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="../../../assets/lib/jquery-wizard/libs/formvalidation/formValidation.min.js"></script>
+        <script type="text/javascript" src="../../../assets/lib/jquery-wizard/libs/formvalidation/bootstrap.min.js"></script>
+        <script src="../../../assets/js/dataTables/jquery.dataTables.js"></script>
+        <script src="../../../assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script src="../../../assets/js/main.js"></script>
+        <script src="../../../assets/js/validations.js"></script>
+    </body>
+</html>
---
+++ app/view/includes/menu.php
@@ -0,0 +1,33 @@
+<body>
+    <header>
+        <nav class="navbar navbar-black" role="navigation">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img src="../../../assets/img/TALogo.png"></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="fa fa-bars"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse navbar-right" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="../../view/index/home.php">Inicio</a></li>
+                    <li><a href="../../view/usuarios/control.usuarios.php">Usuarios</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Categoria #1
+                        <b class="caret"></b>
+                        </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Sub Categoria #1</a></li>
+                        <li><a href="#">Sub Categoria #2</a></li>
+                        <li><a href="#">Sub Categoria #3</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Sub Categoria #4</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="#">Categoria #2</a></li>
+                    <li><a href="#">Categoria #3</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
